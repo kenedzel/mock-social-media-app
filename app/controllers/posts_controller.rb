7d3094b6b83b95b44dcd9fb20e9f287d15703b40
kenneth azarcon <kenneth azarcon>
2016-07-25 18:51:13 +0800

    initial

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+
+  def new
+  	@post = Post.new
+  end
+
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def create
+	@post = Post.new(post_params)
+	@post.user_id = current_user.id
+	if @post.save
+		puts "if"
+		flash[:success] = "Post successfully added"
+		redirect_to root_path
+	else
+		puts "else"
+		flash[:danger] = "cant post."
+	end
+  end
+
+  	private
+	def post_params
+		params.require(:post).permit(:user_id, :content)
+	end
+
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  def index
+
+  end
+
+  def new
+  	@post = Post.new
+  end
+end
